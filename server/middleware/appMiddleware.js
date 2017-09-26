5195d278090102befc2ddf46371b2c621213e47e
Elsis Sitati <sitatielsis@gmail.com>
2017-09-26 15:14:25 +0300

    seeting up middlewares

---
+++ server/middleware/appMiddleware.js
@@ -0,0 +1,8 @@
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+
+module.exports = function(app){
+    app.use(morgan('dev'));
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+};
