e30f76b25800bdb8696e7b3a209d19f706b08ab6
Elsis Sitati <sitatielsis@gmail.com>
2017-10-21 15:42:18 +0300

    set up routes to work with controllers

--- server/api/category/categoryRoutes.js
+++ server/api/category/categoryRoutes.js
@@ -1,9 +1,17 @@
 let router = require('express').Router();
+let controller = require('./categoryController');
+
+router.param('id', controller.params);
 
 router.route('/')
-    .get(function(req, res){
-        console.log("Hey how you doing")
-        res.send("This is how we rock")
-    })
+    .get(controller.get)
+    .post(controller.post);
+
+router.route('/:id')
+    .get(controller.getOne)
+    .put(controller.put)
+    .delete(controller.delete);
+
+
 
 module.exports = router;
