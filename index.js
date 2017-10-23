55b19bf19a382327c490916e33b5eb24a6885db1
Elsis Sitati <sitatielsis@gmail.com>
2017-10-23 08:25:18 +0300

    add mongodb connection

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ let app = require('./server/server');
 
 
 app.listen(config.port);
-console.log('listening on http://loacast:' + config.port)
+console.log('listening on http://localhost:' + config.port)
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 let express = require('express');
 let app = express();
 let api = require('./api/api');
-let err = require('./middleware/error')
+let err = require('./middleware/error');
+let config = require('./config/config');
+let mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/blog-api');
+mongoose.connect(config.db.url);
 
 let setUpMiddleware = require('./middleware/appMiddleware');
 setUpMiddleware(app);
